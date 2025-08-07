e0ab75ae5989755fbdaec7ff70feff9c9f812be8
barba-12 <barbaciniriccardo@gmail.com>
2025-08-07 19:56:20 +0200

    modifica nome repo in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personal-web-site/',
+  base: '/barba-12.github.io/',
   plugins: [react()],
 })
